0b25aee14d58f593436d14ec5b35de99773133b4
biauxflo <florestan.biaux@etu.utc.fr>
2022-10-25 19:29:10 +0200

    feat : Bases du POC IHM

---
+++ vs/LO23/Client/ihm-main/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Client.ihm_main.Views;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Client.ihm_main.ViewModel
+{
+    class MainWindowViewModel
+    {
+        private ICommand openWindow;
+        public ICommand OpenWindow
+        {
+            get => openWindow;
+            set
+            {
+                openWindow = value;
+            }
+        }
+
+        private SecondWindowView secondWindow;
+
+        public MainWindowViewModel()
+        {
+            this.openWindow = new RelayCommand(OpenSecondWindow, true);
+        }
+
+        private void OpenSecondWindow()
+        {
+            secondWindow = new SecondWindowView();
+            secondWindow.DataContext = new SecondWindowViewModel();
+            secondWindow.Show();
+        }
+    }
+}
