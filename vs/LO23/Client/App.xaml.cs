c9d77abcda57a5e25f95ef65c7e36bfcd17d3b92
biauxflo <florestan.biaux@etu.utc.fr>
2022-10-25 19:29:10 +0200

    feat : Bases du POC IHM

--- vs/LO23/Client/App.xaml.cs
+++ vs/LO23/Client/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Client.ihm_main.Views;
+using Client.ihm_main.ViewModel;
 
 namespace Client
 {
@@ -13,5 +15,12 @@ namespace Client
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow window = new MainWindow();
+            window.DataContext = new MainWindowViewModel();
+            window.Show();
+        }
     }
 }
