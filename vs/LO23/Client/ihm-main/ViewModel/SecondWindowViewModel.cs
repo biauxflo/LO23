e2340bf534625a468d0408c42aaee729f1a4a0b5
biauxflo <florestan.biaux@etu.utc.fr>
2022-11-02 17:13:35 +0100

    Feat : Add dependency between the windows of the POC

--- vs/LO23/Client/ihm-main/ViewModel/SecondWindowViewModel.cs
+++ vs/LO23/Client/ihm-main/ViewModel/SecondWindowViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Client.ihm_main.ViewModel
@@ -21,15 +22,48 @@ namespace Client.ihm_main.ViewModel
             }
         }
 
-        public SecondWindowViewModel()
+        private ICommand onClosing;
+        public ICommand OnClosing
+        {
+            get => onClosing;
+            set
+            {
+                onClosing = value;
+            }
+        }
+
+        private MainWindowViewModel parentViewModel;
+        
+        private MainWindow parentWindow;
+        public MainWindow ParentWindow
+        {
+            get => parentWindow;
+            set
+            {
+                parentWindow = value;
+                parentViewModel = (MainWindowViewModel)parentWindow.DataContext;
+            }
+        }
+
+        public SecondWindowViewModel(MainWindow win)
         {
             this.closeWindow = new RelayCommand<object>(CloseSecondWindow, true);
+            this.onClosing = new RelayCommand(OnWindowClosing, true);
+            ParentWindow = win;
+            parentViewModel.SecondWindowIsNotActive = false;
         }
 
         private void CloseSecondWindow(object view)
         {
             SecondWindowView window = (SecondWindowView)view;
             window.Close();
+            OnWindowClosing();
+        }
+
+        private void OnWindowClosing()
+        {
+            parentWindow.WindowState = WindowState.Normal;
+            parentViewModel.SecondWindowIsNotActive = true;
         }
     }
 }
