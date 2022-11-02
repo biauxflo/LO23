a7689e08797ebf0dced772b16384472d1b056c1a
biauxflo <florestan.biaux@etu.utc.fr>
2022-11-02 17:13:35 +0100

    Feat : Add dependency between the windows of the POC

--- vs/LO23/Client/ihm-main/ViewModel/MainWindowViewModel.cs
+++ vs/LO23/Client/ihm-main/ViewModel/MainWindowViewModel.cs
@@ -2,9 +2,12 @@
 using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Client.ihm_main.ViewModel
@@ -21,18 +24,41 @@ namespace Client.ihm_main.ViewModel
             }
         }
 
+        private bool secondWindowIsNotActive = true;
+        public bool SecondWindowIsNotActive
+        {
+            get => secondWindowIsNotActive;
+            set
+            {
+                secondWindowIsNotActive = value;
+                NotifyPropertyChanged("SecondWindowIsNotActive");
+            }
+        }
+
         private SecondWindowView secondWindow;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public MainWindowViewModel()
         {
-            this.openWindow = new RelayCommand(OpenSecondWindow, true);
+            this.openWindow = new RelayCommand<object>(OpenSecondWindow, SecondWindowIsNotActive);
         }
 
-        private void OpenSecondWindow()
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
         {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void OpenSecondWindow(object view)
+        {
+            MainWindow win = (MainWindow)view;
             secondWindow = new SecondWindowView();
-            secondWindow.DataContext = new SecondWindowViewModel();
+            secondWindow.DataContext = new SecondWindowViewModel(win);
             secondWindow.Show();
+            win.WindowState = WindowState.Minimized;
         }
     }
 }
