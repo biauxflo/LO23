923952dc6a552d32255700c90ce9e07a3f1e6291
kbouabda <kenza.bouabdallah@etu.utc.fr>
2022-11-05 13:46:17 +0100

    WIP : ecran de connexion

---
+++ vs/LO23/Client/ihm-main/DTO/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.ihm_main.DTO
+{
+    class User
+    {
+        private string login { get; set; }
+        private string password { get; set; }
+
+        public User(string log, string pass) {
+            login = log;
+            password = pass;
+        }
+
+        public User() {
+            login = string.Empty;
+            password = string.Empty;
+        }
+    }
+}
---
+++ vs/LO23/Client/ihm-main/ViewModels/ConnectionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.ihm_main.DTO;
+
+namespace Client.ihm_main
+{
+    class ConnectionViewModel
+    {
+        public User user1 = new User();
+        public User user2 = new User("utilisateur", "utilisateur123");
+    }
+}
