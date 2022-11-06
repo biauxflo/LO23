e9a7a0dc2caa60136569fee99861993568f7a00c
Florestan Biaux <florestan.biaux@etu.utc.fr>
2022-11-06 03:27:30 +0100

    refacto : Harmonisation de l'emplacement de la POC pour correspondre à celle des autres modules

--- sandbox/poc-ihm-main/App.xaml.cs
+++ sandbox/poc-ihm-main/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Client
+namespace poc_ihm_main
 {
     /// <summary>
     /// Logique d'interaction pour App.xaml
