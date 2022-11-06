a31daa47cee39bf13d230693efcea1c23050cbd4
Florestan Biaux <florestan.biaux@etu.utc.fr>
2022-11-06 03:27:30 +0100

    refacto : Harmonisation de l'emplacement de la POC pour correspondre à celle des autres modules

--- sandbox/poc-ihm-main/Views/MainWindow.xaml.cs
+++ sandbox/poc-ihm-main/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Client.ihm_main.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Client.ihm_main.Views
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
