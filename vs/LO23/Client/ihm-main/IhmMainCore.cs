0308c8a137b202db621642ff5a414b01c415afea
Gauthier Roy <gauthier.roy@etu.utc.fr>
2022-11-08 19:15:26 +0100

    debut creation fichiers

--- vs/LO23/Client/ihm-main/IhmMainCore.cs
+++ vs/LO23/Client/ihm-main/IhmMainCore.cs
@@ -20,19 +20,24 @@ namespace Client.ihm_main
         #region Déclaration des pages et de leurs viewModels
 
         /// <summary>
-        /// Page de connection
+        /// Page de connection.
         /// </summary>
         private readonly Page connectionPage = new ConnectionView();
 
-        internal void GameListUpdated(Game game)
-        {
-            throw new NotImplementedException();
-        }
-
         /// <summary>
-        /// View Model de la page de connexion
+        /// View Model de la page de connexion.
         /// </summary>
         private readonly ConnectionViewModel connectionViewModel = new ConnectionViewModel();
+
+        /// <summary>
+        /// Page de creation de Partie.
+        /// </summary>
+        private readonly Page gameCreationPage = new GameCreationView();
+
+        /// <summary>
+        /// View Model de creation de Partie.
+        /// </summary>
+        private readonly GameCreationViewModel gameCreationViewModel = new GameCreationViewModel();
 
         /// <summary>
         /// Page d'acceuil une fois connecté.
@@ -51,6 +56,7 @@ namespace Client.ihm_main
             //Association des vues et de leur view model
             mainWindow.DataContext = mainWindowViewModel;
             connectionPage.DataContext = connectionViewModel;
+            gameCreationPage.DataContext = gameCreationViewModel;
             homePage.DataContext = homeViewModel;
 
             // Page active de la fenetre
---
+++ vs/LO23/Client/ihm-main/ViewModels/GameCreationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.ihm_main.DTO;
+using GalaSoft.MvvmLight.CommandWpf;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Client.ihm_main.ViewModels
+{
+    internal class GameCreationViewModel
+    {
+
+    }
+}
