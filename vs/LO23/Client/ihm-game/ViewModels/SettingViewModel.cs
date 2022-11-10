14642259fc537cd0e80c32441f3203a934c07e47
evangoet <eliot.van-goethem@etu.utc.fr>
2022-11-10 12:33:47 +0100

    [ADD] dossier pour commencer implémentation

---
+++ vs/LO23/Client/ihm-game/ViewModels/SettingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.ihm_game.ViewModels
+{
+    class Class2
+    {
+    }
+}
