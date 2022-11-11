1776ace60a8741f93eb8cb04332bc6daeb4e3e4b
Merwane Bouri <merwane.bouri@etu.utc.fr>
2022-11-11 20:22:10 +0100

    Initialisation de tous les dossiers et fichiers pour le commencement de ihm-game. GameView juste le bouton paramètre.

--- vs/LO23/Client/App.xaml.cs
+++ vs/LO23/Client/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Client.ihm_game;
+
 
 namespace Client
 {
@@ -13,5 +15,11 @@ namespace Client
     /// </summary>
     public partial class App : Application
     {
+        private IhmGameCore gameCore;
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            gameCore = new IhmGameCore();
+        }
     }
 }
---
+++ vs/LO23/Client/ihm-game/ViewModels/GameViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.CommandWpf;
+
+
+
+
+
+namespace Client.ihm_game.ViewModels
+{
+    internal class GameViewModel
+    {
+        // pour ajouter un bouton, dans le xaml section bouton-> Command="{Binding Path=ParamCommand}"
+        // exemple: <Button Name="BT_parameter"  Grid.Row="0" Grid.Column="0"  BorderThickness="0" Background="#a2aebb" Command="{Binding Path=ParamCommand}">
+        public ICommand ParamCommand { get; set; }
+
+        public GameViewModel()
+        {
+            ParamCommand = new RelayCommand(OnParamClick);
+        }
+        
+        // fonction lié au bouton
+        // mécanisme temporaire juste pour tester affichage d'une console de message
+        private void OnParamClick()
+        {
+            // exemple de fenêtre warning 
+            /* MessageBoxResult result = MessageBox.Show("message dans la fenêtre","nom de la fenêtre", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+            if(result == MessageBoxResult.OK)
+             {
+
+             } */
+            MessageBox.Show("message dans la fenêtre", "nom de la fenêtre", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+        }
+
+    }
+}
