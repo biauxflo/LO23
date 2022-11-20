ec4e39cbac8402d049a6d0b1b55591446d818801
kbouabda <kenza.bouabdallah@etu.utc.fr>
2022-11-20 11:25:09 +0100

    structure de la view

---
+++ vs/LO23/Client/ihm-main/DTO/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.ihm_main.DTO
+{
+    class Game
+    {
+        private string name = string.Empty;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+            }
+        }
+
+        // 1 : partie en cours
+        private int gameStatus = default;
+        public int GameStatus
+        {
+            get => gameStatus;
+            set
+            {
+                gameStatus = value;
+            }
+        }
+
+        public Game()
+        {
+
+        }
+
+        public Game(string name, int gameStatus)
+        {
+            Name = name;
+            GameStatus = gameStatus;
+        }
+    }
+}
