3185de2abf793b9bfc5b55982fa4d4178c0e88c3
Louis Greiner <louis.greiner@etu.utc.fr>
2022-11-22 19:01:24 +0100

    fix : fix merge conflicts

--- vs/LO23/Client/ihm-game/Views/MainWindow.xaml.cs
+++ vs/LO23/Client/ihm-game/Views/MainWindow.xaml.cs
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:sandbox/poc-ihm-main/Views/MainWindow.xaml.cs
 ﻿using Client.ihm_main.ViewModel;
 using System;
 using System.Collections.Generic;
@@ -29,33 +28,3 @@ namespace Client.ihm_main.Views
         }
     }
 }
-========
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Client.ihm_game.Views
-{
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
-}
->>>>>>>> dev:vs/LO23/Client/ihm-game/Views/MainWindow.xaml.cs
