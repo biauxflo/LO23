2185522584e8c061208dcfa2801117ee270eee69
Anaelle Hennion <anaelle.hennion@etu.utc.fr>
2022-12-06 17:23:11 +0100

    add : SettingsView, settingsViewModel and buttons on SettingsView

---
+++ vs/LO23/Client/ihm-game/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.CommandWpf;
+using Shared.data;
+using System.Collections.ObjectModel;
+
+
+namespace Client.ihm_game.ViewModels
+{
+	internal class SettingsViewModel
+	{
+		public ICommand BackToPlayCommand
+		{
+			get; set;
+		}
+		public ICommand SaveCommand
+		{
+			get; set;
+		}
+		public ICommand QuitCommand
+		{
+			get; set;
+		}
+		private readonly IhmGameCore core;
+		public SettingsViewModel(IhmGameCore core)
+		{
+			this.core = core;
+			BackToPlayCommand = new RelayCommand(OnBackToPlayClick);
+			SaveCommand = new RelayCommand(OnSaveClick);
+			QuitCommand = new RelayCommand(OnQuitClick);
+
+
+			Display();
+		}
+
+		// fonction lié au bouton
+		// mécanisme temporaire juste pour tester affichage d'une console de message
+		private void OnBackToPlayClick()
+		{
+			// exemple de fenêtre warning 
+			/* MessageBoxResult result = MessageBox.Show("message dans la fenêtre","nom de la fenêtre", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+            if(result == MessageBoxResult.OK)
+             {
+
+             } */
+			MessageBox.Show("message dans la fenêtre", "nom de la fenêtre", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+		}
+		private void OnSaveClick()
+		{
+			MessageBox.Show("message dans la fenêtre", "nom de la fenêtre", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+		}
+		private void OnQuitClick()
+		{
+			MessageBox.Show("message dans la fenêtre", "nom de la fenêtre", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+		}
+		private void Display()
+		{
+			//Fonctions à remplacer par les fonctions qui seront implémenter dans IHMGameCallsData
+
+		}
+	}
+}
