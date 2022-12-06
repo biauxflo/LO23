939004345abc6e033f6315bc27387681f63da1b6
Eliot Van Goethem <eliot.van-goethem@etu.utc.fr>
2022-12-06 17:01:41 +0000

    Evangoet/feature/idatatogame

---
+++ vs/LO23/Shared/interfaces/IDataToGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.data;
+
+namespace Shared.interfaces
+{
+	public interface IDataToGame
+	{
+		void updateMessageDisplay(ChatMessage message);
+
+		void updateGameDisplay(Game game);
+
+		void startReplayDisplay(Game game);
+
+		void displayGame(Game game);
+	}
+}
