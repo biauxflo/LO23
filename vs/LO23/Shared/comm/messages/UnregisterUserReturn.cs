7dd7e790655493cbd3fb44fa1711e6fc191841d1
Corentin <18225832+CorentinFelten@users.noreply.github.com>
2022-12-13 08:58:14 +0100

    feat: adding new types of messages not existing before

---
+++ vs/LO23/Shared/comm/messages/UnregisterUserReturn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.comm.messages;
+using Shared.interfaces;
+
+namespace Shared.comm
+{
+	public class UnregisterUserReturn : MessageToClient
+	{
+		public override void Handle(ICommToDataClient commToData)
+		{
+			commToData.setLoggedOut();
+			throw new NotImplementedException();
+		}
+	}
+}
