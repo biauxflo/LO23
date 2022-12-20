fa2fa4000cc39ff7e5f1f57eee1b63605ff2f967
Anthony Noir <anthony.noir@etu.utc.fr>
2022-12-20 13:56:54 +0000

    Dev commV2

--- vs/LO23/Shared/comm/MessageToServer.cs
+++ vs/LO23/Shared/comm/MessageToServer.cs
@@ -3,11 +3,18 @@ using System;
 
 namespace Shared.comm
 {
+	/// <summary>
+	/// Message send from the server to the client.
+	/// </summary>
 	public abstract class MessageToServer
 	{
 		/// <summary>
-		/// Operates the process of the message.
+		/// Handles the message. This has to be overridden by children classes.
 		/// </summary>
+		/// <param name="id">Id of the client</param>
+		/// <param name="commToDataServer">Interface which has to be used to process</param>
+		/// <param name="sendTo">Action used to send response</param>
+		/// <param name="broadcastExceptTo">Action used to notify other clients</param>
 		public abstract void Handle(
 			string id,
 			ICommToDataServer commToDataServer,
---
+++ vs/LO23/Shared/comm/messages/GetProfileReturn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.data;
+using Shared.interfaces;
+
+namespace Shared.comm
+{
+	public class GetProfileReturn : MessageToClient
+	{
+		public LightUser user;
+
+		public GetProfileReturn(LightUser user)
+		{
+			this.user = user;
+		}
+
+		public override void Handle(ICommToDataClient commToData)
+		{
+			commToData.getProfileReturn(this.user);
+		}
+	}
+}
