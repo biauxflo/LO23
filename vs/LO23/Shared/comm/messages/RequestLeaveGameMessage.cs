d936698cff21ade18809b1acd59c50bff9063350
Anthony Noir <anthony.noir@etu.utc.fr>
2022-12-20 13:56:54 +0000

    Dev commV2

---
+++ vs/LO23/Shared/comm/messages/RequestLeaveGameMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.comm;
+using Shared.comm.messages;
+using Shared.interfaces;
+using Shared.data;
+
+namespace Shared.comm.messages
+{
+	public class RequestLeaveGameMessage : MessageToServer
+	{
+		public Guid userId;
+		public Guid gameId;
+
+		public RequestLeaveGameMessage(Guid userId, Guid gameId)
+		{
+			this.userId = userId;
+			this.gameId = gameId;
+		}
+		public override void Handle(
+			string id,
+			ICommToDataServer commToDataServer,
+			Action<MessageToClient, string> sendTo,
+			Action<MessageToClient, string> broadcastExceptTo
+			)
+		{
+			Game game = commToDataServer.removePlayerToGame(this.userId, this.gameId);
+			broadcastExceptTo(new NotifyGameChangeMessage(game), id);
+		}
+	}
+}
